3e6460a2d74e5a5707cc3b0fbdaebd76def0df40
WalterTNFC <walter.tad@hotmail.com>
2022-09-18 13:52:39 -0300

    feat: Route createProduct

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import productsRoute from './routes/products';
+import 'express-async-errors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productsRoute);
 
 export default app;
